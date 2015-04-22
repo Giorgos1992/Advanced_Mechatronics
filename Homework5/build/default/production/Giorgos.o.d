1d685fda79de3addd18df872ec7ca07f1407110a
Giorgos Mamakoukas <georgiosmamakoukas2019@u.northwestern.edu>
2015-04-21 22:31:37 -0500

    Accelerometer Bars

---
+++ Homework5/build/default/production/Giorgos.o.d
@@ -0,0 +1,4 @@
+build/default/production/Giorgos.o:  \
+Giorgos.c  \
+i2c_display.h  \
+ i2c_master_int.h 
---
+++ Homework5/build/default/production/Main_Accelerometer.c.o.d
@@ -0,0 +1,5 @@
+build/default/production/Main_Accelerometer.c.o:  \
+Main_Accelerometer.c.c  \
+ i2c_display.h  \
+i2c_master_int.h  \
+accel.h 
