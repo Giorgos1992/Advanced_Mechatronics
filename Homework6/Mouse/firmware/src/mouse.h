6ddbf66207a384a21ec496838310966f0407918b
Giorgos Mamakoukas <georgiosmamakoukas2019@u.northwestern.edu>
2015-04-22 19:32:26 -0500

    Mouse Moves in Circles
    The moves moves in circles

--- Homework6/Mouse/firmware/src/mouse.h
+++ Homework6/Mouse/firmware/src/mouse.h
@@ -1,44 +1,3 @@
-/*******************************************************************************
-  USB HID Function Driver
-
-  Company:
-    Microchip Technology Inc.
-
-  File Name:
-    usb_device_hid_mouse.h
-
-  Summary:
-    USB HID MOUSE Function Driver 
-
-  Description:
-    
-*******************************************************************************/
-
-//DOM-IGNORE-BEGIN
-/*******************************************************************************
-Copyright (c) 2012 released Microchip Technology Inc.  All rights reserved.
-
-Microchip licenses to you the right to use, modify, copy and distribute
-Software only when embedded on a Microchip microcontroller or digital signal
-controller that is integrated into your product or third party product
-(pursuant to the sublicense terms in the accompanying license agreement).
-
-You should refer to the license agreement accompanying this Software for
-additional information regarding your rights and obligations.
-
-SOFTWARE AND DOCUMENTATION ARE PROVIDED AS IS WITHOUT WARRANTY OF ANY KIND,
-EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
-MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
-IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
-CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
-OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
-INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
-CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
-SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
-(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
-*******************************************************************************/
-//DOM-IGNORE-END
-
 #ifndef _USB_FUNCTION_HID_MOUSE_H
 #define _USB_FUNCTION_HID_MOUSE_H
 
@@ -51,53 +10,9 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 #include "usb/usb_device.h"
 #include "usb/usb_device_hid.h"
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: USB Device HID Mouse types and definitions
-// *****************************************************************************
-// *****************************************************************************
-
-// *****************************************************************************
-/* Number of Mouse Buttons.
-
-  Summary:
-    Number of Mouse Buttons.
-
-  Description:
-    Number of Mouse Buttons.
-
-  Remarks:
-    None.
-*/
 #define MOUSE_BUTTON_NUMBERS 2
 
-// *****************************************************************************
-/* Mouse Coordinate.
-
-  Summary:
-    Mouse Coordinate type
-
-  Description:
-    This type defines the  Mouse Coordinate data type.
-    
-  Remarks:
-    None.
-*/
-
 typedef int8_t MOUSE_COORDINATE; 
-
-// *****************************************************************************
-/*  Mouse Button State.
-
-  Summary:
-   Mouse Button State.
-
-  Description:
-    This enumeration defines the possible state of the mouse button.
-    
-  Remarks:
-    None.
-*/
 
 typedef enum
 {
@@ -110,34 +25,12 @@ typedef enum
 }
 MOUSE_BUTTON_STATE;
 
-// *****************************************************************************
-/* Mouse Report
-
-  Summary:
-   Mouse Report.
-
-  Description:
-    This is the Mouse Report. The application can use the 
-    MOUSE_ReportCreate() function to populate this report.
-    
-  Remarks:
-    None.
-*/
-
 typedef struct
 {
     uint8_t data[3];
 }
 MOUSE_REPORT;
 
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: USB HID Mouse functions 
-// *****************************************************************************
-// *****************************************************************************
-
-// *****************************************************************************
 /* Function:
     void MOUSE_ReportCreate
     (
@@ -155,9 +48,6 @@ MOUSE_REPORT;
     to create the mouse report. The report can then be sent to the host by using
     the USB_DEVICE_HID_ReportSend() function.
 
-  Precondition:
-    None.
-
   Parameters:
     x - Mouse X Coordinate
 
@@ -167,9 +57,6 @@ MOUSE_REPORT;
     defined by USB_HID_MOUSE_BUTTON_NUBMERS.
 
     mouseReport - Output only variable that will contain the mouse report.
-
-  Returns:
-    None
 
   Example:
     <code>
@@ -210,10 +97,6 @@ MOUSE_REPORT;
     // The completion of the report send is indicated by the 
     // USB_DEVICE_HID_EVENT_REPORT_SEND_COMPLETE event.
 
-    </code>
-
-  Remarks:
-    None.
 */
 
 void MOUSE_ReportCreate
--- Homework6/Mouse/firmware/src/system_config/pic32mx_usb_sk2_int_dyn/system_init.c
+++ Homework6/Mouse/firmware/src/system_config/pic32mx_usb_sk2_int_dyn/system_init.c
@@ -1,108 +1,41 @@
-/*******************************************************************************
-  System Initialization File
-
-  File Name:
-    system_init.c
-
-  Summary:
-    This file contains source code necessary to initialize the system.
-
-  Description:
-    This file contains source code necessary to initialize the system.  It
-    implements the "SYS_Initialize" function, configuration bits, and allocates
-    any necessary global system resources, such as the systemObjects structure
-    that contains the object handles to all the MPLAB Harmony module objects in
-    the system.
- *******************************************************************************/
-
-// DOM-IGNORE-BEGIN
-/*******************************************************************************
-Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
-
-Microchip licenses to you the right to use, modify, copy and distribute
-Software only when embedded on a Microchip microcontroller or digital signal
-controller that is integrated into your product or third party product
-(pursuant to the sublicense terms in the accompanying license agreement).
-
-You should refer to the license agreement accompanying this Software for
-additional information regarding your rights and obligations.
-
-SOFTWARE AND DOCUMENTATION ARE PROVIDED AS IS WITHOUT WARRANTY OF ANY KIND,
-EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
-MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
-IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
-CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
-OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
-INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
-CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
-SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
-(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
- *******************************************************************************/
-// DOM-IGNORE-END
-
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Included Files
-// *****************************************************************************
-// *****************************************************************************
-
 #include "system_config.h"
 #include "system_definitions.h"
 #include "app.h"
 
-
-// ****************************************************************************
-// ****************************************************************************
-// Section: Configuration Bits
-// ****************************************************************************
-// ****************************************************************************
-
-/*** DEVCFG0 ***/
-
-#pragma config DEBUG =      OFF
-#pragma config ICESEL =     ICS_PGx2
-#pragma config PWP =        0xff
-#pragma config BWP =        OFF
-#pragma config CP =         OFF
-
-/*** DEVCFG1 ***/
-
-#pragma config FNOSC =      PRIPLL
-#pragma config FSOSCEN =    OFF
-#pragma config IESO =       OFF
-#pragma config POSCMOD =    XT
-#pragma config OSCIOFNC =   OFF
-#pragma config FPBDIV =     DIV_1
-#pragma config FCKSM =      CSDCMD
-#pragma config WDTPS =      PS1048576
-#pragma config FWDTEN =     OFF
-
-/*** DEVCFG2 ***/
-
-#pragma config FPLLIDIV =   DIV_2
-#pragma config FPLLMUL =    MUL_20
-#pragma config FPLLODIV =   DIV_1
-#pragma config UPLLIDIV =   DIV_2
-#pragma config UPLLEN =     ON
-
-/*** DEVCFG3 ***/
-
-#pragma config USERID =     0xffff
-#pragma config FSRSSEL =    PRIORITY_7
-#pragma config FMIIEN =     OFF
-#pragma config FETHIO =     OFF
-#pragma config FCANIO =     OFF
-#pragma config FUSBIDIO =   OFF
-#pragma config FVBUSONIO =  OFF
-
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Library/Stack Initialization Data
-// *****************************************************************************
-// *****************************************************************************/
-
+// <editor-fold defaultstate="collapsed" desc="DEVCFGs here">
+/* ***********************DEVCFGs here ********************** */
+// DEVCFG0
+#pragma config DEBUG = OFF // no debugging
+#pragma config JTAGEN = OFF // no jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // no write protect
+#pragma config BWP = OFF // not boot write protect
+#pragma config CP = OFF // no code protect
+// DEVCFG1
+#pragma config FNOSC = PRIPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // turn off secondary oscillator
+#pragma config IESO = OFF // no switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config OSCIOFNC = OFF // free up secondary osc pins by turning sosc off
+#pragma config FPBDIV = DIV_1 // divide CPU freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // do not enable clock switch
+#pragma config WDTPS = PS1048576 // slowest wdt
+#pragma config WINDIS = OFF // no wdt window
+#pragma config FWDTEN = OFF // wdt off by default
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+// DEVCFG2 - get the CPU clock to 40MHz
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz:8MHz/2
+#pragma config FPLLMUL = MUL_20 // multiply clock after FPLLIDIV:4*20 = 80MHz
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 40MHz:80/2 = 40MHz
+#pragma config UPLLIDIV = DIV_2 // divide 8MHz input clock, then multiply by 12 to get 48MHz for USB
+#pragma config UPLLEN = ON // USB clock on
+// DEVCFG3
+#pragma config USERID = 0 // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = ON // allow only one reconfiguration
+#pragma config IOL1WAY = ON // allow only one reconfiguration
+#pragma config FUSBIDIO = OFF // USB pins controlled by USB module
+#pragma config FVBUSONIO = OFF // controlled by USB module
+// </editor-fold>
 
 /****************************************************
  * Class specific descriptor - HID Report descriptor
@@ -163,12 +96,6 @@ const USB_DEVICE_FUNCTION_REGISTRATION_TABLE funcRegistrationTable[1] =
     },
 };
 
-/*******************************************
- * USB Device Layer Descriptors
- *******************************************/
-/*******************************************
- *  USB Device Desciptor for this demo
- *******************************************/
 const USB_DEVICE_DESCRIPTOR fullSpeedDeviceDescriptor =
 {
     0x12,                       // Size of this descriptor in bytes
@@ -366,32 +293,8 @@ const USB_DEVICE_INIT usbDevInitData =
 
 };
 
-
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Driver Initialization Data
-// *****************************************************************************
-// *****************************************************************************
-
-
-
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: System Data
-// *****************************************************************************
-// *****************************************************************************
-
 /* Structure to hold the object handles for the modules in the system. */
 SYSTEM_OBJECTS sysObj;
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Module Initialization Data
-// *****************************************************************************
-// *****************************************************************************
-
 
 /*** System Device Control Initialization Data ***/
 
@@ -399,32 +302,6 @@ const SYS_DEVCON_INIT sysDevconInit =
 {
     .moduleInit = {0},
 };
-
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Static Initialization Functions
-// *****************************************************************************
-// *****************************************************************************
-
-
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: System Initialization
-// *****************************************************************************
-// *****************************************************************************
-
-/*******************************************************************************
-  Function:
-    void SYS_Initialize ( SYS_INIT_DATA *data )
-
-  Summary:
-    Initializes the board, services, drivers, application and other modules.
-
-  Remarks:
-    See prototype in system/common/sys_module.h.
- */
 
 void SYS_Initialize ( void* data )
 {
@@ -440,11 +317,6 @@ void SYS_Initialize ( void* data )
     /* System Services Initialization */    
     SYS_INT_Initialize();  
     SYS_PORTS_Initialize();
-
-    /* Initialize Drivers */
-
-    /* Initialize System Services */
-
 
     /* Initialize Middleware */
     /* Set priority of USB interrupt source */
@@ -464,8 +336,3 @@ void SYS_Initialize ( void* data )
     /* Initialize the Application */
     APP_Initialize();
 }
-
-/*******************************************************************************
- End of File
-*/
-
