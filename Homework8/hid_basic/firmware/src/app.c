0b2d83178cbbd555420c4e593eaa5c6199fcc88d
Giorgos Mamakoukas <georgiosmamakoukas2019@u.northwestern.edu>
2015-04-27 17:01:02 -0500

    Lights Toggle and LED prints successfully

--- Homework8/hid_basic/firmware/src/app.c
+++ Homework8/hid_basic/firmware/src/app.c
@@ -1,95 +1,15 @@
-/*******************************************************************************
-  MPLAB Harmony Application Source File
-  
-  Company:
-    Microchip Technology Inc.
-  
-  File Name:
-    app.c
-
-  Summary:
-    This file contains the source code for the MPLAB Harmony application.
-
-  Description:
-    This file contains the source code for the MPLAB Harmony application.  It 
-    implements the logic of the application's state machine and it may call 
-    API routines of other MPLAB Harmony modules in the system, such as drivers,
-    system services, and middleware.  However, it does not call any of the
-    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
-    the modules in the system or make any assumptions about when those functions
-    are called.  That is the responsibility of the configuration-specific system
-    files.
- *******************************************************************************/
-
-// DOM-IGNORE-BEGIN
-/*******************************************************************************
-Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
-
-Microchip licenses to you the right to use, modify, copy and distribute
-Software only when embedded on a Microchip microcontroller or digital signal
-controller that is integrated into your product or third party product
-(pursuant to the sublicense terms in the accompanying license agreement).
-
-You should refer to the license agreement accompanying this Software for
-additional information regarding your rights and obligations.
-
-SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
-EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
-MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
-IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
-CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
-OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
-INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
-CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
-SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
-(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
- *******************************************************************************/
-// DOM-IGNORE-END
-
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Included Files 
-// *****************************************************************************
-// *****************************************************************************
-
 #include "system_definitions.h"
 #include "app.h"
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: Global Data Definitions
-// *****************************************************************************
-// *****************************************************************************
-
+ char Message[20];
+ 
 /* Recieve data buffer */
 uint8_t receiveDataBuffer[64] APP_MAKE_BUFFER_DMA_READY;
 
 /* Transmit data buffer */
 uint8_t  transmitDataBuffer[64] APP_MAKE_BUFFER_DMA_READY;
 
-// *****************************************************************************
-/* Application Data
-
-  Summary:
-    Holds application data
-
-  Description:
-    This structure holds the application's data.
-
-  Remarks:
-    This structure should be initialized by the APP_Initialize function.
-    
-    Application strings and buffers are be defined outside this structure.
-*/
-
 APP_DATA appData;
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Callback Functions
-// *****************************************************************************
-// *****************************************************************************
 
 USB_DEVICE_HID_EVENT_RESPONSE APP_USBDeviceHIDEventHandler
 (
@@ -178,9 +98,9 @@ void APP_USBDeviceEventHandler(USB_DEVICE_EVENT event, void * eventData, uintptr
              * Hence close handles to all function drivers (Only if they are
              * opened previously. */
 
-            BSP_LEDOn  (APP_USB_LED_1);
+            BSP_LEDOff  (APP_USB_LED_1);
             BSP_LEDOn  (APP_USB_LED_2);
-            BSP_LEDOff (APP_USB_LED_3);
+//            BSP_LEDOff (APP_USB_LED_3);
             appData.deviceConfigured = false;
             appData.state = APP_STATE_WAIT_FOR_CONFIGURATION;
             break;
@@ -196,18 +116,21 @@ void APP_USBDeviceEventHandler(USB_DEVICE_EVENT event, void * eventData, uintptr
             USB_DEVICE_HID_EventHandlerSet(USB_DEVICE_HID_INDEX_0, APP_USBDeviceHIDEventHandler, (uintptr_t)&appData);
 
             /* Update the LEDs */
+
             BSP_LEDOff (APP_USB_LED_1);
             BSP_LEDOff (APP_USB_LED_2);
-            BSP_LEDOn  (APP_USB_LED_3);
+//            BSP_LEDOn  (APP_USB_LED_3);
 
+
+           
             break;
 
         case USB_DEVICE_EVENT_SUSPENDED:
 
             /* Switch on green and orange, switch off red */
-            BSP_LEDOff (APP_USB_LED_1);
-            BSP_LEDOn  (APP_USB_LED_2);
-            BSP_LEDOn  (APP_USB_LED_3);
+            BSP_LEDOn (APP_USB_LED_1);
+            BSP_LEDOff  (APP_USB_LED_2);
+//            BSP_LEDOn  (APP_USB_LED_3);
             break;
 
         case USB_DEVICE_EVENT_POWER_DETECTED:
@@ -231,30 +154,6 @@ void APP_USBDeviceEventHandler(USB_DEVICE_EVENT event, void * eventData, uintptr
     }
 }
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Local Functions
-// *****************************************************************************
-// *****************************************************************************
-
-/* TODO:  Add any necessary local functions.
-*/
-
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Initialization and State Machine Functions
-// *****************************************************************************
-// *****************************************************************************
-
-/*******************************************************************************
-  Function:
-    void APP_Initialize ( void )
-
-  Remarks:
-    See prototype in app.h.
- */
-
 void APP_Initialize ( void )
 {
     /* Place the App state machine in its initial state. */
@@ -269,15 +168,6 @@ void APP_Initialize ( void )
     appData.receiveDataBuffer = &receiveDataBuffer[0];
     appData.transmitDataBuffer = &transmitDataBuffer[0];
 }
-
-
-/******************************************************************************
-  Function:
-    void APP_Tasks ( void )
-
-  Remarks:
-    See prototype in app.h.
- */
 
 void APP_Tasks (void )
 {
@@ -308,7 +198,7 @@ void APP_Tasks (void )
             break;
 
         case APP_STATE_WAIT_FOR_CONFIGURATION:
-
+            
             if(appData.deviceConfigured == true)
             {
                 /* Device is ready to run the main task */
@@ -333,7 +223,7 @@ void APP_Tasks (void )
             {
                 /* Look at the data the host sent, to see what
                  * kind of application specific command it sent. */
-
+            
                 switch(appData.receiveDataBuffer[0])
                 {
                     case 0x80:
@@ -341,6 +231,10 @@ void APP_Tasks (void )
                         /* Toggle on board LED1 to LED2. */
                         BSP_LEDToggle( APP_USB_LED_1 );
                         BSP_LEDToggle( APP_USB_LED_2 );
+
+
+                        writemessage("Giorgos",28,32);
+                        display_draw();
 
                         appData.hidDataReceived = false;
 
@@ -399,9 +293,3 @@ void APP_Tasks (void )
             break;
     }
 }
- 
-
-/*******************************************************************************
- End of File
- */
-
